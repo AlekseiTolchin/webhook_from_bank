f4b81f4c528df6d387b3b8108792c56ed473cd95
Aleksey525 <alex_tolchin@mail.ru>
2025-05-28 18:10:05 +0300

    Register models

--- src/organization/admin.py
+++ src/organization/admin.py
@@ -1,1 +1,18 @@
 from django.contrib import admin
+
+from .models import Organization, Payment, BalanceLog
+
+
+@admin.register(Organization)
+class OrganizationAdmin(admin.ModelAdmin):
+    list_display = ('inn', 'balance')
+
+
+@admin.register(Payment)
+class PaymentAdmin(admin.ModelAdmin):
+    list_display = ('operation_id', 'amount')
+
+
+@admin.register(BalanceLog)
+class PaymentAdmin(admin.ModelAdmin):
+    list_display = ('organization', 'amount', 'new_balance', 'date')
